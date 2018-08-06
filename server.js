e16c0110503884d7acbda6cc7ee15ce9144747cb
Pai <phagcartorn@gmail.com>
2018-08-06 22:00:05 +0700

    init project

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+
+var app = express();
+
+// HBS
+hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerHelper("getCurrentYear",()=>{
+
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper("ToUpperCase",(text)=>{
+ 
+    return text.toUpperCase();
+
+});
+
+app.set("view engine","hbs");
+
+// Middle ware
+app.use(express.static(__dirname + "/public"));
+app.use((req,res,next)=>{
+
+    var date = new Date().toDateString();
+    var log =  "Application run at :" + date + " Method :" + req.method +  " Path : " + req.path
+
+    fs.appendFile('server.log',log + "\n" , (err)=>{
+      if(err){
+          console.log("Can not log");
+      }
+
+    });
+
+    console.log(log);
+    next();
+});
+
+
+app.get("/",(req,res)=>{
+
+    res.render("home.hbs",
+    {pageTitle:"Home page"}
+);
+   // res.send({status:"OK",message:"No results"});
+});
+
+app.get("/about",(req,res)=>{
+
+    res.render("about.hbs",{pageTitle:"About page"});
+   // res.send({status:"OK",message:"About page"});
+
+});
+
+
+app.get("/bad",(req,res)=>{
+
+    res.send({status:"Failed",message:"Can not find the page"});
+
+});
+
+app.listen(3000,()=>{
+
+    console.log("server start on port 3000");
+
+});
